103bda76ed3ef41339275b1362fdba8993523ed7
red krow <redanabil14@gmail.com>
2023-04-20 19:01:48 +0000

    working on mlx settings

---
+++ includes/cub3d.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:27:40 by rnabil            #+#    #+#             */
+/*   Updated: 2023/04/20 18:42:59 by rnabil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/errno.h>
+# include <math.h>
+# include <string.h>
+# include "../../mlx_linux/mlx.h"
+
+#ifndef GAME_HEIGHT
+# define GAME_HEIGHT 1024
+#endif
+
+#ifndef GAME_WIDTH
+# define GAME_WIDTH 1024
+#endif
+
+typedef struct s_global_settings
+{
+	t_mlx_settings	mlxset;
+	
+}			 	t_global_settings;
+
+typedef struct s_img
+{
+	void		*img;
+	int			*arr;
+}				t_img;
+
+typedef struct s_mlx_settings
+{
+	void		*mlx;
+	void		*win_ptr;
+	t_img		*img;
+}				t_mlx_settings;
+#endif
---
+++ src/cub3d.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:34:14 by rnabil            #+#    #+#             */
+/*   Updated: 2023/04/20 19:00:20 by rnabil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static int	set_up_game(t_global_settings *game)
+{
+	game->mlxset.mlx = mlx_init();
+	game->mlxset.win_ptr = mlx_new_window(game->mlxset.mlx, GAME_WIDTH, GAME_HEIGHT, "LBENJENSTEIN64");
+	game->mlxset.img->img = mlx_new_image(game->mlxset.mlx, GAME_WIDTH, GAME_HEIGHT);
+	// game->img.arr = (int *)mlx_get_data_addr(game->img.ptr, &(game->img.bpp),&(game->img.len), &(game->img.endian));
+	game->mlxset.img->arr = (int *)mlx_get_data_addr(game->mlxset.img->img, NULL, NULL, NULL);
+}
+
+int main(int argc, char **argv)
+{
+	t_global_settings game;
+	
+	//argc protection
+	//map reading
+	//map parsing
+	set_up_game(&game);
+}
