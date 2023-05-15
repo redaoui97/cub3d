021d047bdb448ce00f329a64ec35b5550190290d
Nabil Reda <redanabil14@gmail.com>
2023-05-15 23:55:54 +0100

    reworking parser and fixing bugs

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:34:14 by rnabil            #+#    #+#             */
-/*   Updated: 2023/04/28 15:05:33 by rnabil           ###   ########.fr       */
+/*   Updated: 2023/05/15 23:48:45 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ int main(int argc, char **argv)
 {
 	t_global_settings game;
 	
+	/*add this to parser*/
+	(void)argv;
 	if (argc != 2)
 		fatal_error("Program must have 1 argument!");
 	/*game init*/
 	if (set_up_game(&game) == EXIT_FAILURE)
 		fatal_error("Error setting up mlx settings!");
-	(void)argv;
 	/*map check*/
 	//add protection
 	//map_check(argv[1]);
