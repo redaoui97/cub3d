ef69104139f968182bb82598b43359cccddefcd6
Nabil Reda <redanabil14@gmail.com>
2023-05-18 18:02:49 +0100

    merged parser, need to fix some stuff

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:34:14 by rnabil            #+#    #+#             */
-/*   Updated: 2023/05/17 14:11:06 by rnabil           ###   ########.fr       */
+/*   Updated: 2023/05/18 18:01:37 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ int	main(int argc, char **argv)
 		fatal_error("Program must have 1 argument!");
 	if (set_up_game(&game) == EXIT_FAILURE)
 		fatal_error("Error setting up mlx settings!");
-	mlx_hook(game.mlxset.win_ptr, 2, 0, key_press, &game);
-	mlx_loop_hook(game.mlxset.mlx, execution, &game);
-	mlx_loop(game.mlxset.mlx);
+	parse(&game, argv[1]);
+	printf("%d:%d",game.map.ceiling_color, game.map.floor_color);
+	// mlx_hook(game.mlxset.win_ptr, 2, 0, key_press, &game);
+	// mlx_loop_hook(game.mlxset.mlx, execution, &game);
+	// mlx_loop(game.mlxset.mlx);
 	return (0);
 }
